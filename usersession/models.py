fd8506e68383f4cdeddb7ede6e1d13df8dd20b96
Nisha-Mallah <nisha.221ds025@nitk.edu.in>
2025-05-30 12:03:35 +0530

    test changes

--- usersession/models.py
+++ usersession/models.py
@@ -1,24 +1,15 @@
 from django.db import models
 from django.utils import timezone
-<<<<<<< HEAD
 from Users.models import Users
 from chat.models import Chat
 
 
-=======
-from django.db import models
->>>>>>> a2f33f6fe87130ae86aee4dc2a0e2748081876ea
 # Create your models here.
 class userSession(models.Model):
     session_id = models.CharField(max_length=255, unique=True,primary_key=True)
     created_at = models.DateTimeField(timezone.now, editable=False)
     ended_at = models.DateTimeField(blank=True)
-<<<<<<< HEAD
     chats = models.ManyToManyField(Chat, related_name='sessions', blank=True)
     
-=======
-    chatID = models.ForeignKey() 
->>>>>>> a2f33f6fe87130ae86aee4dc2a0e2748081876ea
-
     def __str__(self):
         return f"UserSession(user_id={self.user_id}, session_id={self.session_id})"
