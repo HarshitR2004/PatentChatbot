b5d0d1da32a5609127f500251cf6868d4d6fc74f
Nisha-Mallah <nisha.221ds025@nitk.edu.in>
2025-05-30 13:01:51 +0530

    fixed commit

--- Users/models.py
+++ Users/models.py
@@ -9,4 +9,3 @@ class Users(models.Model):
     email = models.EmailField(unique=True, db_column='email')
     password = models.CharField(max_length=128, db_column='password') 
     created_at = models.DateTimeField(auto_now_add=True, db_column='created_at')
-    user_session = models.ForeignKey(userSession, on_delete=models.CASCADE, db_column='user_session', null=True, blank=True)
