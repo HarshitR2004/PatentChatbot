a2f33f6fe87130ae86aee4dc2a0e2748081876ea
Nisha-Mallah <nisha.221ds025@nitk.edu.in>
2025-05-30 11:43:15 +0530

    updated usersesssion

--- usersession/models.py
+++ usersession/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils import timezone
-
+from django.db import models
 # Create your models here.
 class userSession(models.Model):
     session_id = models.CharField(max_length=255, unique=True,primary_key=True)
     created_at = models.DateTimeField(timezone.now, editable=False)
     ended_at = models.DateTimeField(blank=True)
-    
+    chatID = models.ForeignKey() 
 
     def __str__(self):
         return f"UserSession(user_id={self.user_id}, session_id={self.session_id})"
