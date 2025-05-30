60c250cbb49545165eba1400ec281d37d5dff409
HarshitR2004 <harshithranjan6971@gmail.com>
2025-05-30 12:58:40 +0530

    added apps in settings.py

--- Users/models.py
+++ Users/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from usersession.models import userSession
 
 
 # Create your models here.
@@ -9,4 +8,3 @@ class Users(models.Model):
     email = models.EmailField(unique=True, db_column='email')
     password = models.CharField(max_length=128, db_column='password') 
     created_at = models.DateTimeField(auto_now_add=True, db_column='created_at')
-    user_session = models.ForeignKey(userSession, on_delete=models.CASCADE, db_column='user_session', null=True, blank=True)
