f0b0a70fe81ab6200bf7b212f63dddb11c94e41f
HarshitR2004 <harshithranjan6971@gmail.com>
2025-05-30 11:46:42 +0530

    modified usersession model

--- Users/models.py
+++ Users/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from usersession.models import userSession
+
 
 # Create your models here.
 class Users(models.Model):
     userID = models.AutoField(primary_key=True, db_column='userID')
     username = models.CharField(max_length=150, unique=True, db_column='username')
     email = models.EmailField(unique=True, db_column='email')
-    password = models.CharField(max_length=128, db_column='password')  # Django handles hashing
+    password = models.CharField(max_length=128, db_column='password') 
     created_at = models.DateTimeField(auto_now_add=True, db_column='created_at')
+    user_session = models.ForeignKey(userSession, on_delete=models.CASCADE, db_column='user_session', null=True, blank=True)
--- usersession/models.py
+++ usersession/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from Users.models import Users
+from chat.models import Chat
+
 
 # Create your models here.
 class userSession(models.Model):
     session_id = models.CharField(max_length=255, unique=True,primary_key=True)
     created_at = models.DateTimeField(timezone.now, editable=False)
     ended_at = models.DateTimeField(blank=True)
+    chats = models.ManyToManyField(Chat, related_name='sessions', blank=True)
     
 
     def __str__(self):
