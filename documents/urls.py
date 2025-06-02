1dd1c41ad221497bfb083b5d9b167104e94065ee
Nisha-Mallah <nisha.221ds025@nitk.edu.in>
2025-06-02 12:52:34 +0530

    added api for file viewing

--- documents/urls.py
+++ documents/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     # Route for rendering the documents HTML page
     path('', views.show_documents, name='show_documents'),
     
-    # Route for the API to list all documents
+    # Route for the API to list all documents and create a new document
     path('api/documents/', views.DocumentsListView.as_view(), name='documents-list'),
+    
+    # Route for the API to retrieve, update, or delete a specific document
+    path('api/documents/<int:docid>/', views.DocumentsDetailView.as_view(), name='document-detail'),
 ]
